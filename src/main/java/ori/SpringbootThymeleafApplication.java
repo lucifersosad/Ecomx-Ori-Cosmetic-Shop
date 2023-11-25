2a86bd0662ec354b8a6be2cd94a4d3ca2dcbaeb1
lucifersosad <luciferxhnh@gmail.com>
2023-11-25 16:49:06 +0700

    Config upload file

--- src/main/java/ori/SpringbootThymeleafApplication.java
+++ src/main/java/ori/SpringbootThymeleafApplication.java
@@ -1,13 +1,27 @@
 package ori;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+
+import ori.config.StorageProperties;
+import ori.service.IStorageService;
 
 @SpringBootApplication
+@EnableConfigurationProperties(StorageProperties.class)
 public class SpringbootThymeleafApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootThymeleafApplication.class, args);
+	}
+	
+	@Bean
+	CommandLineRunner init(IStorageService storageService) {
+		return (args -> {
+			storageService.init();
+		});
 	}
 
 }
