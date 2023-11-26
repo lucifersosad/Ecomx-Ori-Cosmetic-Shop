5f692e32171791d3cd0a1390835bad9382d1670d
lucifersosad <luciferxhnh@gmail.com>
2023-11-27 00:08:15 +0700

    fix

--- src/main/java/ori/config/Config.java
+++ src/main/java/ori/config/Config.java
@@ -17,7 +17,7 @@ import jakarta.servlet.http.HttpServletRequest;
 
 public class Config {
 	public static String vnp_PayUrl = "https://sandbox.vnpayment.vn/paymentv2/vpcpay.html";
-    public static String vnp_ReturnUrl = "http://localhost:8000/vnpay_jsp/vnpay_return.jsp";
+    public static String vnp_ReturnUrl = "http://localhost:8888/vnpay_jsp/vnpay_return.jsp";
     public static String vnp_TmnCode = "M8IEGRE2";
     public static String secretKey = "XGQTYPCIATVOJJNAVFGGGSBTKMKWTSZU";
     public static String vnp_ApiUrl = "https://sandbox.vnpayment.vn/merchant_webapi/api/transaction";
@@ -100,19 +100,6 @@ public class Config {
         } catch (Exception ex) {
             return "";
         }
-    }
-    
-    public static String getIpAddress(HttpServletRequest request) {
-        String ipAdress;
-        try {
-            ipAdress = request.getHeader("X-FORWARDED-FOR");
-            if (ipAdress == null) {
-                ipAdress = request.getRemoteAddr();
-            }
-        } catch (Exception e) {
-            ipAdress = "Invalid IP:" + e.getMessage();
-        }
-        return ipAdress;
     }
 
     public static String getRandomNumber(int len) {
