288732969ad15f04ff9fd9fb9f74d0f368f415ea
lucifersosad <luciferxhnh@gmail.com>
2023-12-05 18:55:49 +0700

    shopping session

--- src/main/java/ori/repository/BrandRepository.java
+++ src/main/java/ori/repository/BrandRepository.java
@@ -1,4 +1,4 @@
-OrderRepository.javapackage ori.repository;
+package ori.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
