1100c72c2e6d17241a2617b7b379781f156d214e
lucifersosad <luciferxhnh@gmail.com>
2023-12-05 22:18:46 +0700

    Need fixing user

--- src/main/java/ori/entity/ShoppingSession.java
+++ src/main/java/ori/entity/ShoppingSession.java
@@ -5,7 +5,13 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.MapsId;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 @Entity(name = "shopping_session")
 public class ShoppingSession {
 	@EmbeddedId
@@ -23,35 +29,4 @@ public class ShoppingSession {
 	
 	String date;
 
-	public ShoppingSessionKey getId() {
-		return id;
-	}
-
-	public void setId(ShoppingSessionKey id) {
-		this.id = id;
-	}
-
-	public Product getProduct() {
-		return product;
-	}
-
-	public void setProduct(Product product) {
-		this.product = product;
-	}
-
-	public User getUser() {
-		return user;
-	}
-
-	public void setUser(User user) {
-		this.user = user;
-	}
-
-	public String getDate() {
-		return date;
-	}
-
-	public void setDate(String date) {
-		this.date = date;
-	}
 }
