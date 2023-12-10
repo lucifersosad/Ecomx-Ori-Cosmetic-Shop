ecff9484c799ef850c84d771a561c127856576ac
Rennn <trungnghia294@gmail.com>
2023-12-10 14:49:51 +0700

    fix error in statistic

--- src/main/java/ori/repository/OrderRepository.java
+++ src/main/java/ori/repository/OrderRepository.java
@@ -12,18 +12,21 @@ import ori.model.OrderModel;
 
 @Repository
 public interface OrderRepository extends JpaRepository<Order, Integer>{
-	@Query(value = "SELECT IFNULL(sum(total), 0) as total_month FROM `orders`  \r\n"
+	@Query(value = "SELECT IFNULL(sum(CASE WHEN currency=\"VND\" THEN total\r\n"
+			+ "					WHEN currency = \"USD\" THEN total *23000 END ), 0) as total_month FROM `orders`  \r\n"
 			+ "WHERE MONTH(STR_TO_DATE(date, '%d/%m/%Y %H:%i:%s')) = MONTH(CURRENT_DATE()) \r\n"
 			+ "AND status=1 ;", nativeQuery = true)
     int revenueOnCurrentMonth();
-	@Query(value = "SELECT IFNULL(sum(total), 0) as total_month FROM `orders`  \r\n"
+	@Query(value = "SELECT IFNULL(sum(CASE WHEN currency=\"VND\" THEN total\r\n"
+			+ "					WHEN currency = \"USD\" THEN total *23000 END ), 0) as total_month FROM `orders`  \r\n"
 			+ "WHERE YEAR(STR_TO_DATE(date, '%d/%m/%Y %H:%i:%s')) = YEAR(CURRENT_DATE()) \r\n"
 			+ "AND status=1 ;", nativeQuery = true)
     int revenueOnCurrentYear();
 	@Query(value = "SELECT IFNULL(sum(status)/count(*),0)*100 as completed_rate\r\n"
 			+ "FROM `orders` \r\n", nativeQuery = true)
     int rateCompleted();
-	@Query(value = "SELECT IFNULL(sum(total), 0) as total_month FROM `orders`  \r\n"
+	@Query(value = "SELECT IFNULL(sum(CASE WHEN currency=\"VND\" THEN total\r\n"
+			+ "					WHEN currency = \"USD\" THEN total *23000 END ), 0) as total_month FROM `orders`  \r\n"
 			+ "WHERE QUARTER(STR_TO_DATE(date, '%d/%m/%Y %H:%i:%s')) = QUARTER(CURRENT_DATE()) \r\n"
 			+ "AND status=1 ;", nativeQuery = true)
     int revenueOnCurrentQuarter();
